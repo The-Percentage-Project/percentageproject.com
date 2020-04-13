c4fa9bfb1c2d000fb1972f28424dd99645bc8e4b
Jordan Lei <thunderflash.lei@gmail.com>
2020-04-13 03:08:47 -0400

    Updates

--- Gemfile
+++
@@ -1,4 +0,0 @@
-source 'https://rubygems.org'
-
-gem "jekyll", ">= 3.8.4"
-gem 'wdm', '>= 0.1.0' if Gem.win_platform?
---
+++ precache-manifest.2c75021c12c04f6999f7c54ccde1b66c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9498c192347df68ad2cf5a5fa05634f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5c544d94c3c8e29f836",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "2b09429d5ef572c81678",
+    "url": "/static/css/main.ed92f37a.chunk.css"
+  },
+  {
+    "revision": "c5c544d94c3c8e29f836",
+    "url": "/static/js/2.b538d5f1.chunk.js"
+  },
+  {
+    "revision": "8be83cedca90ca2c1b68e5d028b8742c",
+    "url": "/static/js/2.b538d5f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b09429d5ef572c81678",
+    "url": "/static/js/main.8c5c9217.chunk.js"
+  },
+  {
+    "revision": "a796a236ee8470d38315",
+    "url": "/static/js/runtime-main.783779e9.js"
+  },
+  {
+    "revision": "6d453b5e3ce0e1c2473d15c90f03def3",
+    "url": "/static/media/datawelcome-mobile.6d453b5e.svg"
+  },
+  {
+    "revision": "a44eca88209b9c281af87f110fcbce19",
+    "url": "/static/media/datawelcome.a44eca88.svg"
+  },
+  {
+    "revision": "678f9ebb8a02f03cb9672c76656bfc46",
+    "url": "/static/media/datawelcome1.678f9ebb.svg"
+  },
+  {
+    "revision": "d2b24388d66b02faac0e25713fd6fa49",
+    "url": "/static/media/dp_header.d2b24388.JPG"
+  },
+  {
+    "revision": "7143b9f793dcea188faa89336207d790",
+    "url": "/static/media/duke_header.7143b9f7.JPG"
+  },
+  {
+    "revision": "30fa0886f8b6840a53f5deb91f9722f5",
+    "url": "/static/media/logo-insta.30fa0886.png"
+  },
+  {
+    "revision": "49952e8323d4a5b12a1854770779b274",
+    "url": "/static/media/logo-twitter.49952e83.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "b4f35a59cf359f63740d23f6807f80dc",
+    "url": "/static/media/menu-logo.b4f35a59.png"
+  },
+  {
+    "revision": "c3df2b785b5325cfc4c671f3ffae1450",
+    "url": "/static/media/paypal_transparent.c3df2b78.png"
+  },
+  {
+    "revision": "a55cc0778272366e7041d97a336e38fe",
+    "url": "/static/media/sponsorswelcome.a55cc077.svg"
+  },
+  {
+    "revision": "446e138b72db5644b1172b4970ddc9b8",
+    "url": "/static/media/welcome-mobile.446e138b.svg"
+  },
+  {
+    "revision": "da6ef72696402e15aab04055e9c7b267",
+    "url": "/static/media/welcome.da6ef726.svg"
+  },
+  {
+    "revision": "959b2f095cb732899eb7797f4e994ee5",
+    "url": "/static/media/welcome1.959b2f09.svg"
+  },
+  {
+    "revision": "6ccf407941c528d2d9a8b837a10e32e5",
+    "url": "/static/media/welcome2.6ccf4079.svg"
+  }
+]);
